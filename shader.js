9165ea16e7b046cfe9f025221296c20daa05ad29
RizkyAnggita <13519132@std.stei.itb.ac.id>
2022-02-20 23:19:12 +0700

    modularisasi

---
+++ shader.js
@@ -0,0 +1,53 @@
+var vertexShaderText = 
+[
+    'precision mediump float;',
+    'attribute vec2 vertPosition;',
+    'uniform vec2 u_resolution;',
+    '',
+    'void main()',
+    '{',
+    'vec2 zeroToOne = vertPosition / u_resolution;',
+    'vec2 zeroToTwo = zeroToOne * 2.0;',
+    'vec2 clipSpace = zeroToTwo - 1.0;',
+    'gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);',
+    '}'
+].join('\n');
+
+var fragmentShaderText = 
+[
+    'precision mediump float;',
+    '',
+    'uniform vec4 u_color;',
+    'void main()',
+    '{',
+    'gl_FragColor = u_color;',
+    '}'
+
+].join('\n');
+
+function createShader(gl, type, source) {
+    var shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+      return shader;
+    }
+   
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+      return program;
+    }
+   
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
