712665259b5737b22ae2c85f64f15ae990000b53
RizkyAnggita <13519132@std.stei.itb.ac.id>
2022-02-26 00:08:12 +0700

    fix checkNearGaris and checkJarak function

--- line.js
+++ line.js
@@ -5,7 +5,11 @@ function createGaris(gl, x1, x2, y1, y2) {
 }
 
 function checkNearGaris(arrPosObject, x, y) {
-    if ((x >= arrPosObject[0]-10 && x <= arrPosObject[1]+10) && (y >= arrPosObject[2]-10 && y <= arrPosObject[3]+10)) {
+    var maxX = Math.max(arrPosObject[0], arrPosObject[1])
+    var minX = Math.min(arrPosObject[0], arrPosObject[1])
+    var maxY = Math.max(arrPosObject[1], arrPosObject[2])
+    var minY = Math.min(arrPosObject[1], arrPosObject[2])
+    if ((x >= minX-10 && x <= maxX+10) || (y >= minY-10 && y <= maxY+10)) {
         return true
     }
 }
@@ -20,10 +24,10 @@ function translasiGaris(arrPos, arrMidPoint, deltaX, deltaY) {
     return [arrPos, arrMidPoint]
 }
 
-function checkJarak(arrPosObject, x) {
-    let jarakX1 = Math.abs(x - arrPosObject[0])
-    let jarakX2 = Math.abs(x - arrPosObject[1])
-    if (Math.min(jarakX1, jarakX2) == jarakX1) {
+function checkJarak(arrPosObject, x, y) {
+    let jarakX1Y1 = Math.pow(x - arrPosObject[0], 2) + Math.pow(y - arrPosObject[2], 2)
+    let jarakX2Y2 = Math.pow(x - arrPosObject[1], 2) + Math.pow(y - arrPosObject[3], 2)
+    if (Math.min(jarakX1Y1, jarakX2Y2) == jarakX1Y1) {
         return true
     }
 }
