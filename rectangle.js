b01398535a62fc556211a14ec9ccd8028e11c642
RizkyAnggita <13519132@std.stei.itb.ac.id>
2022-02-26 16:13:51 +0700

    new function checkNearPersegi dan fix render persegi/panjang

--- rectangle.js
+++ rectangle.js
@@ -1,15 +1,15 @@
-function createPersegiPanjang(gl, x1, x2, y1, y2) {
+// function createPersegiPanjang(gl, x1, x2, y1, y2) {
 
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2]), gl.STATIC_DRAW)
-}
+//     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+//         x1, y1,
+//         x2, y1,
+//         x1, y2,
+//         x1, y2,
+//         x2, y1,
+//         x2, y2]), gl.STATIC_DRAW)
+// }
 
-function createPersegiPanjang2(gl, x1, y1, x2, y2, x3, y3, x4, y4) {
+function createPersegiPanjang(gl, x1, y1, x2, y2, x3, y3, x4, y4) {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
         x1, y1,
         x2, y2,
@@ -21,18 +21,35 @@ function createPersegiPanjang2(gl, x1, y1, x2, y2, x3, y3, x4, y4) {
 }
 
 function checkInsidePersegiPanjang(arrPosObject, x, y) {
-    if ((x >= arrPosObject[0] && x <= arrPosObject[1]) && (y >= arrPosObject[2] && y <= arrPosObject[3])) {
+    if ((x >= arrPosObject[0] && x <= arrPosObject[2]) && (y >= arrPosObject[1] && y <= arrPosObject[5])) {
         return true
     }
 }
 
+function checkNearPersegiPanjang(arrPosObject, x, y) {
+    var jarak2Titik = 0
+    for (let i = 0; i < arrPosObject.length; i = i + 2) {
+        jarak2Titik = Math.pow(x - arrPosObject[i], 2) + Math.pow(y - arrPosObject[i+1], 2)
+        if (jarak2Titik <= 200) {
+            return [true, i, i+1]
+        }    
+    }
+     return [false, -1, -1]
+
+}
+
+
 function translasiPersegiPanjang(arrPos, arrMidPoint, deltaX, deltaY) {
     arrPos[0] = arrPos[0] + deltaX
-    arrPos[1] = arrPos[1] + deltaX
-    arrPos[2] = arrPos[2] + deltaY
+    arrPos[2] = arrPos[2] + deltaX
+    arrPos[4] = arrPos[4] + deltaX
+    arrPos[6] = arrPos[6] + deltaX
+    arrPos[1] = arrPos[1] + deltaY
     arrPos[3] = arrPos[3] + deltaY
-    arrMidPoint[0] = (arrPos[0] + arrPos[1]) / 2
-    arrMidPoint[1] = (arrPos[2] + arrPos[3]) / 2
+    arrPos[5] = arrPos[5] + deltaY
+    arrPos[7] = arrPos[7] + deltaY
+    arrMidPoint[0] = (arrPos[0] + arrPos[2]) / 2
+    arrMidPoint[1] = (arrPos[1] + arrPos[5]) / 2
     return [arrPos, arrMidPoint]
 }
 
